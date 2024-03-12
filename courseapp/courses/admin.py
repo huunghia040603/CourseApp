f35be73eafb19267dacb52487ccc0c7026098bb1
huunghia040603 <huunghia040603@gmail.com>
2024-03-12 16:02:17 +0700

    hi

---
+++ courseapp/courses/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.utils.html import mark_safe
+from courses.models import Course,Category
+
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+class CourseForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorUploadingWidget)
+    class Meta:
+       model = Course
+       fields = '__all__'
+
+class MyCourseAdmin(admin.ModelAdmin):
+    list_display = ['id','name','created_date','updated_date','active']
+    search_fields = ['id','name']
+    list_filter = ['created_date','name']
+    readonly_fields = ['my_image']
+    form = CourseForm
+
+    def my_image(self,course):
+        if course.image:
+            return mark_safe(f"<img src='/static/{course.image.name}' width='200'/>")
+
+
+admin.site.register(Category)
+admin.site.register(Course,MyCourseAdmin)
+
+# Register your models here.
